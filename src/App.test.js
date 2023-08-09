87328527d8812e55380e3d83116c63b05193b49d
Bruno Tineli <bruno.tineli@gmail.com>
2023-08-09 00:05:54 -0300

    Tests + unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,74 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor  } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let globalContainer;
+
+describe("App tests", () => {
+  describe("Page elements for initial App state", () => {
+    beforeEach(() => {
+      const { container } = render(<App />);
+      globalContainer = container;
+    });
+
+    test('Page title', () => {  
+      const element = screen.getByText('Get Github Repos');
+      expect(element).toBeInTheDocument();
+    });
+    
+    test('Label input', () => {  
+      const element = screen.getByLabelText('Github Username');
+      expect(element).toBeInTheDocument();
+    });
+    
+    test('Search input', () => { 
+      const element = screen.getByRole('textbox', {name: "Github Username"});
+      expect(element).toBeInTheDocument();
+    });
+    
+    test('Search button', () => {  
+      const element = screen.getByRole('button', {name: "Go"});
+      expect(element).toHaveTextContent('Go');
+    });
+
+    test('Empty search results', () => {  
+      const element = globalContainer.querySelector('.output-status-text');
+      expect(element).toHaveTextContent('No repos');
+    });
+  })
+
+  describe("Search", () => {
+    beforeEach(() => {
+      const { container } = render(<App />);
+      globalContainer = container;
+    });
+
+    test('Search when send Enter', async () => {
+      await userEvent.type(screen.getByRole('textbox', {name: "Github Username"}), "{enter}")
+      expect(globalContainer.querySelector('.circle')).toBeInTheDocument();
+    });
+
+    test('Search when click on search button', async () => {
+      await userEvent.click(screen.getByRole('button', {name: "Go"}))
+      expect(globalContainer.querySelector('.circle')).toBeInTheDocument();
+    });
+
+    test('Successful search', async () => {
+      await userEvent.type(screen.getByRole('textbox', {name: "Github Username"}), "brunotineli{enter}")
+      await waitFor(
+        () => expect(screen.getByText(/Found [0-9]+ Repos/i)).toBeInTheDocument(),
+        {timeout: 10000}
+      );
+    });
+
+    test('Results rendered when click on search button', async () => {
+      await userEvent.click(screen.getByRole('button', {name: "Go"}))
+      expect(globalContainer.querySelector('.circle')).toBeInTheDocument();
+      await waitFor(
+        () => expect(screen.getByText('Github user not found')).toBeInTheDocument(),
+        {timeout: 10000}
+      );
+    });
+  })
+})
--- testable-flows.md
+++ testable-flows.md
@@ -1,1 +1,34 @@
-(Add your list of flows here)
+# Tests
+
+
+## Tests based on the requirements
+
+The focus here is to cover every acceptance criteria item.
+
+Scenarios:
+1. *Required page elements: page title, search input, search button, and search result section
+1. *User flow for successful search results: 0 repo, 1 repo, N repos, and reports w/o description
+1. *User flow for failure search results: user not found and unexpected error like network issues
+
+*Highest priority scenarios because they are related to the core feature of the application
+
+## Tests based on exploratory testing
+
+The focus here is to explore based on the continuous usage of the application
+
+Sessions with the help of the test heuristics:
+1. Input data: empty, too short, too big, special chars, etc.
+1. Multibrowser: Firefox and Chrome
+1. Responsiveness: different browser windows sizes
+1. Multidevices: test on Desktop and Mobile browsers
+1. Non-functional insights: run LightHouse for performance and accessibility insights
+
+# Test Strategy
+
+## Unit Tests
+
+The unit tests will cover mainly the page elements and form submit event in order to keep the page behavior
+
+## System Tests
+
+The system tests will cover the user flow mainly the search flows and their results.
